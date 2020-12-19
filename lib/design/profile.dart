233d6f83d350e639b7c57538870b3b042e9ed526
Abhishek Daberao <47315164+abhidaberao@users.noreply.github.com>
2020-12-19 16:56:51 +0530

    Update profile.dart

--- lib/design/profile.dart
+++ lib/design/profile.dart
@@ -1,7 +1,7 @@
 
 
 import 'package:firebase_database/firebase_database.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter_web/material.dart';
 import 'package:flutter/semantics.dart';
 import 'package:myapp/database/provider.dart';
 import 'package:myapp/design/rew_icon_icons.dart';
