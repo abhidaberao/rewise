61d2df31b52d7558599db571c7dd164ee25a4d97
Abhishek Daberao <47315164+abhidaberao@users.noreply.github.com>
2020-12-19 22:28:54 +0530

    Update main.dart

--- web/main.dart
+++ web/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_web_ui/ui.dart' as ui;
 
 // TODO: change `my_app` to refer to your app package name.
-import 'package:my_app/main.dart' as app;
+import 'package:rewise3/main.dart' as app;
 
 main() async {
   await ui.webOnlyInitializePlatform();
